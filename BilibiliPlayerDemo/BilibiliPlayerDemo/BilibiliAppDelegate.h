e75d74749a3c4618ceee309e1f570844b9c645ef
Mountain Xiang <783600983@qq.com>
2018-04-18 15:33:29 +0800

    adding the Pods directory to your .gitignore; IJKMediaFramework.framework is Large File(134MB),so git forbid upload large file; Initialize Project

---
+++ BilibiliPlayerDemo/BilibiliPlayerDemo/BilibiliAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BilibiliPlayerDemo
+//
+//  Created by MountainX on 2018/4/18.
+//  Copyright © 2018年 MTX Software Technology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BilibiliAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BilibiliPlayerDemo/BilibiliPlayerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BilibiliPlayerDemo
+//
+//  Created by MountainX on 2018/4/18.
+//  Copyright © 2018年 MTX Software Technology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BilibiliAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BilibiliAppDelegate class]));
+    }
+}
